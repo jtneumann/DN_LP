f78f351519b56fd83c221b67031b63b29250827e
Jeff Neumann <jtneumann@hotmail.com>
2020-05-25 21:17:43 -0400

    list comprehension method

---
+++ IteratingThruFoldersForPdfs.py
@@ -0,0 +1,13 @@
+﻿import os
+
+def getPdfs(dirName):
+    allPdfs = []
+    for root, dirs, files in os.walk(dirName):
+        for file in files:
+            if file.endswith('.pdf'):
+                allPdfs.append(file)
+    return allPdfs
+
+dirName = '.'
+pdfs = getPdfs(dirName)
+print(pdfs)
