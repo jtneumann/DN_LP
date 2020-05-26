0bcf3a306e23e2d8bd0c2df5396bf4572a0dd4bc
Jeff Neumann <jtneumann@hotmail.com>
2020-05-25 21:19:52 -0400

    renaming per assignment direction

---
+++ dn.py
@@ -0,0 +1,9 @@
+﻿import os
+
+def getPdfs(dirName):
+    allPdfs = [file for root, dirs, files in os.walk(dirName) for file in files if file.endswith('.pdf')]
+    return allPdfs
+    
+dirName = '.'
+pdfs = getPdfs(dirName)
+print(pdfs)
